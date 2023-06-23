12b86baf3fad24fb92577596f5a70775972c4930
quentin452 <42176772+quentin452@users.noreply.github.com>
2023-06-23 21:09:39 +0200

    Revert micro optimize + fix crash with vintagefix
    crash :
    
    java.lang.NullPointerException: Exception in server tick loop
        at org.embeddedt.vintagefix.fastmap.PropertyIndexer$EnumIndexer.toIndex(PropertyIndexer.java:144)
        at org.embeddedt.vintagefix.fastmap.PropertyIndexer$EnumIndexer.toIndex(PropertyIndexer.java:129)
        at org.embeddedt.vintagefix.fastmap.FastMapKey.getInternalIndex(FastMapKey.java:53)
        at org.embeddedt.vintagefix.fastmap.BinaryFastMapKey.toPartialMapIndex(BinaryFastMapKey.java:51)
        at org.embeddedt.vintagefix.fastmap.BinaryFastMapKey.replaceIn(BinaryFastMapKey.java:41)
        at org.embeddedt.vintagefix.fastmap.FastMap.with(FastMap.java:72)
        at org.embeddedt.vintagefix.fastmap.FastMap.withUnsafe(FastMap.java:134)
        at net.minecraft.block.state.BlockStateContainer$StateImplementation.redirect$bam000$getNeighborFromFastMap(BlockStateContainer.java:616)
        at net.minecraft.block.state.BlockStateContainer$StateImplementation.withProperty(BlockStateContainer.java:223)
        at net.minecraft.block.BlockDoublePlant.placeAt(BlockDoublePlant.java:150)
        at net.minecraft.world.gen.feature.WorldGenDoublePlant.generate(SourceFile:29)
        at kipster.nt.biomes.warm.BiomeHeath.addDoublePlants(BiomeHeath.java:96)
        at kipster.nt.biomes.warm.BiomeHeath.decorate(BiomeHeath.java:128)
        at net.minecraft.world.gen.ChunkGeneratorOverworld.populate(ChunkGeneratorOverworld.java:464)
        at net.minecraft.world.chunk.Chunk.populate(Chunk.java:1019)
        at net.minecraft.world.chunk.Chunk.populate(Chunk.java:999)
        at net.minecraft.world.gen.ChunkProviderServer.provideChunk(ChunkProviderServer.java:157)
        at net.minecraft.server.MinecraftServer.initialWorldChunkLoad(MinecraftServer.java:344)
        at net.minecraft.server.integrated.IntegratedServer.loadAllWorlds(IntegratedServer.java:142)
        at net.minecraft.server.integrated.IntegratedServer.init(IntegratedServer.java:156)
        at net.minecraft.server.MinecraftServer.run(MinecraftServer.java:486)
        at java.lang.Thread.run(Thread.java:750)

--- src/main/java/kipster/nt/biomes/warm/BiomeBlossomForest.java
+++ src/main/java/kipster/nt/biomes/warm/BiomeBlossomForest.java
@@ -1,182 +1,183 @@
 package kipster.nt.biomes.warm;
 
-import kipster.nt.world.gen.trees.WorldGenTreeBigPurpleCherry;
-import kipster.nt.world.gen.trees.WorldGenTreeCherryPink;
-import kipster.nt.world.gen.trees.WorldGenTreeCherryWhite;
-import net.minecraft.block.BlockDoublePlant;
-import net.minecraft.block.BlockFlower;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.passive.EntityRabbit;
-import net.minecraft.entity.passive.EntityWolf;
+import java.util.Random;
+
+import kipster.nt.biomes.BiomeInit;
+import kipster.nt.biomes.warm.BiomeBlossomForest.EmeraldGenerator;
+import kipster.nt.world.gen.trees.*;
+import net.minecraft.init.Biomes;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.BiomeForest;
+import net.minecraft.world.biome.Biome.BiomeProperties;
 import net.minecraft.world.gen.feature.WorldGenAbstractTree;
 import net.minecraft.world.gen.feature.WorldGenLakes;
 import net.minecraft.world.gen.feature.WorldGenerator;
+import net.minecraft.block.BlockFlower;
+import net.minecraft.block.BlockDoublePlant;
+import net.minecraft.entity.passive.EntityRabbit;
+import net.minecraft.entity.passive.EntityWolf;
+import net.minecraftforge.common.BiomeManager;
 import net.minecraftforge.event.terraingen.DecorateBiomeEvent;
 
-import java.util.Random;
-
-public class BiomeBlossomForest extends Biome 
+public class BiomeBlossomForest extends Biome
 {
-	private static final int EMERALD_COUNT = 10;
-	private static final int EMERALD_COUNT_RANGE = 6;
-	private static final int OFFSET = net.minecraftforge.common.ForgeModContainer.fixVanillaCascading ? 8 : 0;
-	private static final int MAX_XZ = 16;
-	private static final int MAX_Y = 28;
-	private static final int FLAG = 16 | 2;
-	private static final WorldGenLakes LAKE = new WorldGenLakes(Blocks.WATER);
-	private static final WorldGenAbstractTree WHITE_TREE = new WorldGenTreeCherryWhite(false, false);
-	private static final WorldGenAbstractTree PINK_TREE = new WorldGenTreeCherryPink(false, false);
-	private static final WorldGenTreeBigPurpleCherry purpleCherryGenerator = new WorldGenTreeBigPurpleCherry(false);
+
+	protected static final WorldGenLakes LAKE = new WorldGenLakes(Blocks.WATER);
+	protected static final WorldGenAbstractTree WHITE_TREE = new WorldGenTreeCherryWhite(false, false);
+	protected static final WorldGenAbstractTree PINK_TREE = new WorldGenTreeCherryPink(false, false);
+	private final WorldGenTreeBigPurpleCherry purpleCherryGenerator = new WorldGenTreeBigPurpleCherry(false);
 
 	public BiomeBlossomForest(BiomeProperties properties)
-	{	
+	{
 		super(properties);
-		
+
 		topBlock = Blocks.GRASS.getDefaultState();
 		fillerBlock = Blocks.DIRT.getDefaultState();
-		
+
 		this.decorator.treesPerChunk = 8;
 		this.decorator.flowersPerChunk = 100;
-        this.decorator.grassPerChunk = 1;
-	    this.decorator.generateFalls = true;
-	    
-	    this.spawnableCreatureList.add(new Biome.SpawnListEntry(EntityWolf.class, 5, 4, 4));
-        this.spawnableCreatureList.add(new Biome.SpawnListEntry(EntityRabbit.class, 4, 2, 3));
-        this.flowers.clear();
-        for (BlockFlower.EnumFlowerType type : BlockFlower.EnumFlowerType.values())
-        {
-            if (type.getBlockType() == BlockFlower.EnumFlowerColor.YELLOW) continue;
-            if (type == BlockFlower.EnumFlowerType.BLUE_ORCHID) type = BlockFlower.EnumFlowerType.POPPY;
-            addFlower(net.minecraft.init.Blocks.RED_FLOWER.getDefaultState().withProperty(net.minecraft.init.Blocks.RED_FLOWER.getTypeProperty(), type), 10);
-        }
-	}
-	private static final BlockFlower.EnumFlowerType[] FLOWER_TYPES = BlockFlower.EnumFlowerType.values();
+		this.decorator.grassPerChunk = 1;
+		this.decorator.generateFalls = true;
 
-	public BlockFlower.EnumFlowerType pickRandomFlower(Random rand, BlockPos pos) {
+		this.spawnableCreatureList.add(new Biome.SpawnListEntry(EntityWolf.class, 5, 4, 4));
+		this.spawnableCreatureList.add(new Biome.SpawnListEntry(EntityRabbit.class, 4, 2, 3));
+		this.flowers.clear();
+		for (BlockFlower.EnumFlowerType type : BlockFlower.EnumFlowerType.values())
+		{
+			if (type.getBlockType() == BlockFlower.EnumFlowerColor.YELLOW) continue;
+			if (type == BlockFlower.EnumFlowerType.BLUE_ORCHID) type = BlockFlower.EnumFlowerType.POPPY;
+			addFlower(net.minecraft.init.Blocks.RED_FLOWER.getDefaultState().withProperty(net.minecraft.init.Blocks.RED_FLOWER.getTypeProperty(), type), 10);
+		}
+	}
+	public BlockFlower.EnumFlowerType pickRandomFlower(Random rand, BlockPos pos)
+	{
 		double d0 = MathHelper.clamp((1.0D + GRASS_COLOR_NOISE.getValue((double)pos.getX() / 48.0D, (double)pos.getZ() / 48.0D)) / 2.0D, 0.0D, 0.9999D);
-		BlockFlower.EnumFlowerType blockflower$enumflowertype = FLOWER_TYPES[(int)(d0 * (double)FLOWER_TYPES.length)];
+		BlockFlower.EnumFlowerType blockflower$enumflowertype = BlockFlower.EnumFlowerType.values()[(int)(d0 * (double)BlockFlower.EnumFlowerType.values().length)];
 		return blockflower$enumflowertype == BlockFlower.EnumFlowerType.BLUE_ORCHID ? BlockFlower.EnumFlowerType.POPPY : blockflower$enumflowertype;
 	}
 	@Override
 	public WorldGenAbstractTree getRandomTreeFeature(Random rand) {
-	if (rand.nextInt(2) > 0)
-	{
-		  return WHITE_TREE;
+		if (rand.nextInt(2) > 0)
+		{
+			return WHITE_TREE;
+		}
+		else
+		{
+			return (WorldGenAbstractTree)(rand.nextInt(4) == 0 ? this.purpleCherryGenerator : PINK_TREE);
+		}
 	}
-	else
+
+	public void addDoublePlants(World p_185378_1_, Random p_185378_2_, BlockPos p_185378_3_, int p_185378_4_)
 	{
-	  return rand.nextInt(4) == 0 ? this.purpleCherryGenerator : PINK_TREE;
-	}
-}
+		for (int i = 0; i < p_185378_4_; ++i)
+		{
+			int j = p_185378_2_.nextInt(3);
 
-	public void addDoublePlants(World worldIn, Random rand, BlockPos pos, int p_185378_4_) {
-		BlockDoublePlant.EnumPlantType syringaType = BlockDoublePlant.EnumPlantType.SYRINGA;
-		BlockDoublePlant.EnumPlantType roseType = BlockDoublePlant.EnumPlantType.ROSE;
-		BlockDoublePlant.EnumPlantType paeoniaType = BlockDoublePlant.EnumPlantType.PAEONIA;
-
-		for (int i = 0; i < p_185378_4_; ++i) {
-			int j = rand.nextInt(3);
-
-			if (j == 0) {
-				DOUBLE_PLANT_GENERATOR.setPlantType(syringaType);
-			} else if (j == 1) {
-				DOUBLE_PLANT_GENERATOR.setPlantType(roseType);
-			} else if (j == 2) {
-				DOUBLE_PLANT_GENERATOR.setPlantType(paeoniaType);
+			if (j == 0)
+			{
+				DOUBLE_PLANT_GENERATOR.setPlantType(BlockDoublePlant.EnumPlantType.SYRINGA);
+			}
+			else if (j == 1)
+			{
+				DOUBLE_PLANT_GENERATOR.setPlantType(BlockDoublePlant.EnumPlantType.ROSE);
+			}
+			else if (j == 2)
+			{
+				DOUBLE_PLANT_GENERATOR.setPlantType(BlockDoublePlant.EnumPlantType.PAEONIA);
 			}
 
-			for (int k = 0; k < 5; ++k) {
-				int l = rand.nextInt(16) + 8;
-				int i1 = rand.nextInt(16) + 8;
-				int j1 = rand.nextInt(worldIn.getHeight(pos.add(l, 0, i1)).getY() + 32);
+			for (int k = 0; k < 5; ++k)
+			{
+				int l = p_185378_2_.nextInt(16) + 8;
+				int i1 = p_185378_2_.nextInt(16) + 8;
+				int j1 = p_185378_2_.nextInt(p_185378_1_.getHeight(p_185378_3_.add(l, 0, i1)).getY() + 32);
 
-				if (DOUBLE_PLANT_GENERATOR.generate(worldIn, rand, new BlockPos(pos.getX() + l, j1, pos.getZ() + i1))) {
+				if (DOUBLE_PLANT_GENERATOR.generate(p_185378_1_, p_185378_2_, new BlockPos(p_185378_3_.getX() + l, j1, p_185378_3_.getZ() + i1)))
+				{
 					break;
 				}
 			}
 		}
 	}
-    
+
 	public void decorate(World worldIn, Random rand, BlockPos pos)
 	{
-		 net.minecraftforge.common.MinecraftForge.ORE_GEN_BUS.post(new net.minecraftforge.event.terraingen.OreGenEvent.Pre(worldIn, rand, pos));
-	        WorldGenerator emeralds = new EmeraldGenerator();
-	        if (net.minecraftforge.event.terraingen.TerrainGen.generateOre(worldIn, rand, emeralds, pos, net.minecraftforge.event.terraingen.OreGenEvent.GenerateMinable.EventType.EMERALD))
-	            emeralds.generate(worldIn, rand, pos);
-	        
-		 if (net.minecraftforge.event.terraingen.TerrainGen.decorate(worldIn, rand, pos, DecorateBiomeEvent.Decorate.EventType.LAKE_WATER)) {
-	         int boulderChance = rand.nextInt(12);
-	         if (boulderChance == 0) {
-	          int k6 = rand.nextInt(16) + 8;
-	          int l = rand.nextInt(16) + 8;
-	           BlockPos blockpos = worldIn.getHeight(pos.add(k6, 0, l));
-	           LAKE.generate(worldIn, rand, blockpos);
-	         }
-	         net.minecraftforge.common.MinecraftForge.ORE_GEN_BUS.post(new net.minecraftforge.event.terraingen.OreGenEvent.Post(worldIn, rand, pos));
-	        }
-		 DOUBLE_PLANT_GENERATOR.setPlantType(BlockDoublePlant.EnumPlantType.GRASS);
+		net.minecraftforge.common.MinecraftForge.ORE_GEN_BUS.post(new net.minecraftforge.event.terraingen.OreGenEvent.Pre(worldIn, rand, pos));
+		WorldGenerator emeralds = new EmeraldGenerator();
+		if (net.minecraftforge.event.terraingen.TerrainGen.generateOre(worldIn, rand, emeralds, pos, net.minecraftforge.event.terraingen.OreGenEvent.GenerateMinable.EventType.EMERALD))
+			emeralds.generate(worldIn, rand, pos);
 
-		 if(net.minecraftforge.event.terraingen.TerrainGen.decorate(worldIn, rand, pos, net.minecraftforge.event.terraingen.DecorateBiomeEvent.Decorate.EventType.GRASS))
-		 for (int i = 0; i < 7; ++i)
-		 {
-		     int j = rand.nextInt(16) + 8;
-		     int k = rand.nextInt(16) + 8;
-		     int l = rand.nextInt(worldIn.getHeight(pos.add(j, 0, k)).getY() + 32);
-		     DOUBLE_PLANT_GENERATOR.generate(worldIn, rand, pos.add(j, l, k));
-		 }
-		 if(net.minecraftforge.event.terraingen.TerrainGen.decorate(worldIn, rand, pos, net.minecraftforge.event.terraingen.DecorateBiomeEvent.Decorate.EventType.FLOWERS))
-		 { // no tab for patch
-		 int i = rand.nextInt(5) - 3;
-		 {
-		     i += 2;
-		 }
+		if (net.minecraftforge.event.terraingen.TerrainGen.decorate(worldIn, rand, pos, DecorateBiomeEvent.Decorate.EventType.LAKE_WATER)) {
+			int boulderChance = rand.nextInt(12);
+			if (boulderChance == 0) {
+				int k6 = rand.nextInt(16) + 8;
+				int l = rand.nextInt(16) + 8;
+				BlockPos blockpos = worldIn.getHeight(pos.add(k6, 0, l));
+				LAKE.generate(worldIn, rand, blockpos);
+			}
+			net.minecraftforge.common.MinecraftForge.ORE_GEN_BUS.post(new net.minecraftforge.event.terraingen.OreGenEvent.Post(worldIn, rand, pos));
+		}
+		DOUBLE_PLANT_GENERATOR.setPlantType(BlockDoublePlant.EnumPlantType.GRASS);
 
-		 this.addDoublePlants(worldIn, rand, pos, i);
-		 }
-	    super.decorate(worldIn, rand, pos);
-	        }
-	
-	@Override
-   	public int getModdedBiomeGrassColor(int original) {
-   	    return 0x6AD04A;
-   	}
-
-   	@Override
-   	public int getModdedBiomeFoliageColor(int original) {
-   	    return 0x629D30;
-   	}
-
-   	@Override
-	public int getSkyColorByTemp(float currentTemperature) {
-		
-			return 0x49A4EC;
-	}
-
-	
-	public static class EmeraldGenerator extends WorldGenerator
-    {
-		@Override
-		public boolean generate(World worldIn, Random rand, BlockPos pos) {
-			int count = EMERALD_COUNT + rand.nextInt(EMERALD_COUNT_RANGE);
-			int offset = net.minecraftforge.common.ForgeModContainer.fixVanillaCascading ? 8 : 0;
-			IBlockState emeraldOreState = Blocks.EMERALD_ORE.getDefaultState();
-
-			for (int i = 0; i < count; i++) {
-				BlockPos blockpos = pos.add(rand.nextInt(MAX_XZ) + offset, rand.nextInt(MAX_Y) + 2, rand.nextInt(MAX_XZ) + offset);
-				IBlockState state = worldIn.getBlockState(blockpos);
-
-				if (state.getBlock().isReplaceableOreGen(state, worldIn, blockpos, net.minecraft.block.state.pattern.BlockMatcher.forBlock(Blocks.STONE))) {
-					worldIn.setBlockState(blockpos, emeraldOreState, FLAG);
-				}
+		if(net.minecraftforge.event.terraingen.TerrainGen.decorate(worldIn, rand, pos, net.minecraftforge.event.terraingen.DecorateBiomeEvent.Decorate.EventType.GRASS))
+			for (int i = 0; i < 7; ++i)
+			{
+				int j = rand.nextInt(16) + 8;
+				int k = rand.nextInt(16) + 8;
+				int l = rand.nextInt(worldIn.getHeight(pos.add(j, 0, k)).getY() + 32);
+				DOUBLE_PLANT_GENERATOR.generate(worldIn, rand, pos.add(j, l, k));
+			}
+		if(net.minecraftforge.event.terraingen.TerrainGen.decorate(worldIn, rand, pos, net.minecraftforge.event.terraingen.DecorateBiomeEvent.Decorate.EventType.FLOWERS))
+		{ // no tab for patch
+			int i = rand.nextInt(5) - 3;
+			{
+				i += 2;
 			}
 
+			this.addDoublePlants(worldIn, rand, pos, i);
+		}
+		super.decorate(worldIn, rand, pos);
+	}
+
+	@Override
+	public int getModdedBiomeGrassColor(int original) {
+		return 0x6AD04A;
+	}
+
+	@Override
+	public int getModdedBiomeFoliageColor(int original) {
+		return 0x629D30;
+	}
+
+	@Override
+	public int getSkyColorByTemp(float currentTemperature) {
+
+		return 0x49A4EC;
+	}
+
+
+	public static class EmeraldGenerator extends WorldGenerator
+	{
+		@Override
+		public boolean generate(World worldIn, Random rand, BlockPos pos)
+		{
+			int count = 10 + rand.nextInt(6);
+			for (int i = 0; i < count; i++)
+			{
+				int offset = net.minecraftforge.common.ForgeModContainer.fixVanillaCascading ? 8 : 0; // MC-114332
+				BlockPos blockpos = pos.add(rand.nextInt(16) + offset, rand.nextInt(28) + 2, rand.nextInt(16) + offset);
+
+				net.minecraft.block.state.IBlockState state = worldIn.getBlockState(blockpos);
+				if (state.getBlock().isReplaceableOreGen(state, worldIn, blockpos, net.minecraft.block.state.pattern.BlockMatcher.forBlock(Blocks.STONE)))
+				{
+					worldIn.setBlockState(blockpos, Blocks.EMERALD_ORE.getDefaultState(), 16 | 2);
+				}
+			}
 			return true;
 		}
-
 	}
 }
